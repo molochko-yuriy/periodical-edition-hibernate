a42c2fe2ba77fdb6992fb17a998448c367eba821
Molochko <molochko.uriy@gmail.com>
2022-01-08 18:57:38 +0300

    adding property file

--- src/main/java/by/epamtc/periodical_edition/property/Properties.java
+++ src/main/java/by/epamtc/periodical_edition/property/Properties.java
@@ -2,7 +2,11 @@ package by.epamtc.periodical_edition.property;
 
 public class Properties {
     public static final String MIGRATION_LOCATION = "db/migration";
-    public static final String H2_URL = "jdbc:h2:mem:periodical_edition;DB_CLOSE_DELAY=-1";
-    public static final String H2_USER = "sa";
-    public static final String H2_PASSWORD = "";
+    public static final String H2_URL="jdbc:h2:mem:periodical_edition;DB_CLOSE_DELAY=-1";
+    public static final String H2_USER="sa";
+    public static final String H2_PASSWORD ="";
+
+//    public static final String H2_URL = "jdbc:mysql://localhost:3306/periodical_edition_system";
+//    public static final String H2_USER = "root";
+//    public static final String H2_PASSWORD = "VladYura1Java";
 }
